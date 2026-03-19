7fc5a73f6a30cfefdd79be2dd97c4398d96f2eb1
Girjoaba <agirjoaba@student.ethz.ch>
2026-03-19 12:58:52 +0100

    fix: updated the bridge to send actual simulated depth sensor data instead of ground truth pose

--- hal/nautilus_hal/nautilus_hal/external_sensor_sim_bridge.py
+++ hal/nautilus_hal/nautilus_hal/external_sensor_sim_bridge.py
@@ -1,7 +1,8 @@
 import rclpy
-from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import PointStamped
 from py_pkg.uuv_ros_core import UUVTopics, create_publisher_for_topic
 from rclpy.node import Node
+from sensor_msgs.msg import FluidPressure
 from std_msgs.msg import Int32
 
 
@@ -13,11 +14,22 @@ class ExternalSensorSimBridge(Node):
 
         model_name = self.get_parameter("model_name").value
 
-        self.depth_pub = create_publisher_for_topic(self, UUVTopics.EXTERNAL_PRESSURE)
-        self.sim_pose_sub = self.create_subscription(
-            PoseArray,
-            f"/model/{model_name}/pose",
-            self.sim_pose_callback,
+        self.pressure_pub = create_publisher_for_topic(
+            self, UUVTopics.EXTERNAL_PRESSURE
+        )
+        self.depth_pub = create_publisher_for_topic(self, UUVTopics.TEST_EXTERNAL_DEPTH)
+        self.sim_pressure_sub = self.create_subscription(
+            FluidPressure,
+            f"/model/{model_name}/sea_pressure",
+            self.sim_external_pressure_callback,
+            10,
+        )
+        # the topic below is implicitly created by the <Sea Pressure Sensor Plugin>
+        # in model.sdf
+        self.sim_depth_sub = self.create_subscription(
+            PointStamped,
+            f"/model/{model_name}/sea_pressure_depth",
+            self.sim_depth_callback,
             10,
         )
 
@@ -25,11 +37,17 @@ class ExternalSensorSimBridge(Node):
             f"Nautilus Sensor Bridge: Listening for pose on  /model/{model_name}/pose"
         )
 
-    def sim_pose_callback(self, msg):
+    def sim_external_pressure_callback(self, msg):
         """Publish depth as external pressure."""
         depth_msg = Int32()
-        depth_cm = int(-msg.poses[0].position.z * 100)
-        depth_msg.data = depth_cm
+        depth_msg.data = int(msg.fluid_pressure)
+
+        self.pressure_pub.publish(depth_msg)
+
+    def sim_depth_callback(self, msg):
+        """Publish depth as external pressure."""
+        depth_msg = Int32()
+        depth_msg.data = int(msg.point.z * 100)
 
         self.depth_pub.publish(depth_msg)
 
